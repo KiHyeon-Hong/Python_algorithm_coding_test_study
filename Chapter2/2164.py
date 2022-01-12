11d83f72eaf0957830e792227bf672d732f3e1a7
Ki Hyeon Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2022-01-12 11:35:32 +0900

    update code

---
+++ Chapter2/2164.py
@@ -0,0 +1,15 @@
+# 카드2
+
+"""
+from collections import deque
+
+q = deque()
+for _ in range(int(input())):
+  q.append(_ + 1)
+
+while len(q) != 1:
+  q.popleft()
+  q.append(q.popleft())
+
+print(q.popleft())
+"""
--- Chapter2/9012.py
+++ Chapter2/9012.py
@@ -1,2 +1,19 @@
 # 올바른 괄호쌍 문제는 전형적인 Stack을 이용하는 문제이다.
 
+for _ in range(int(input())):
+  stk = []
+  isVPS = True
+
+  for ch in input():
+    if ch == '(':
+      stk.append(ch)
+    else:
+      if len(stk) == 0:
+        isVPS = False
+        break
+      stk.pop()
+  
+  if len(stk) > 0:
+    isVPS = False
+  
+  print('YES' if isVPS else 'NO')
