d2d4097bf16836c7802be2a9513e8a0a8514701f
Ki Hyeon Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2022-01-17 22:03:18 +0900

    update code

--- BinarySearch/10915.py
+++ BinarySearch/10915.py
@@ -1,3 +1,24 @@
+from bisect import bisect_left, bisect_right
+
+N = int(input())
+cards = sorted(list(map(int, input().split())))
+M = int(input())
+qry = list(map(int, input().split()))
+
+ans = []
+for q in qry:
+  l = bisect_left(cards, q)
+  r = bisect_right(cards, q)
+
+  if r - l > 0:
+    ans.append(1)
+  else:
+    ans.append(0)
+
+print(*ans)
+
+
+
 """
 import sys
 input = sys.stdin.readline
@@ -34,22 +55,3 @@ for i in range(M):
 
 print(' '.join(map(str, ans)))
 """
-
-from bisect import bisect_left, bisect_right
-
-N = int(input())
-cards = sorted(list(map(int, input().split())))
-M = int(input())
-qry = list(map(int, input().split()))
-
-ans = []
-for q in qry:
-  l = bisect_left(cards, q)
-  r = bisect_right(cards, q)
-
-  if r - l > 0:
-    ans.append(1)
-  else:
-    ans.append(0)
-
-print(*ans)
